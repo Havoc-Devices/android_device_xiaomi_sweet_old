4c901e717097e98666eeabb97debad70a7b71a52
zaidkhan0997 <kzaid0997@gmail.com>
2021-10-06 00:51:42 +0530

    sweet: HavocOS bringup
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- havoc_sweet.mk
+++ havoc_sweet.mk
@@ -5,17 +5,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common Havoc-OS configurations
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_sweet
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+HAVOC_BUILD_TYPE := Official
+
+PRODUCT_NAME := havoc_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-DEVICE_MAINTAINER := daniml3
-TARGET_INCLUDE_PIXEL_CHARGER := true
-TARGET_BOOT_ANIMATION_RES := 1080
